a159e5ba8ea321f7a7ed77d48dd5cc5a7588f361
Tyler Campbell <thehaqua@gmail.com>
2021-11-29 20:15:21 -0500

    Heroku Setup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Note-Taker
+# 11-Note-Taker
+      
+  A simple webpage that allows the user to take notes.
+      
+  # *Table of Contents
+      
+  * [Installation](#installation)
+  * [License](#license)
+      
+  # Installation
+      
+  To install necessary dependencies, run the following command: 
+      
+  npm install
+      
+      
+  # License
+      
+  ### MIT
+
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/routes');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.listen(PORT, () => 
+console.log(`Listening on PORT: ${PORT}`)
+);
